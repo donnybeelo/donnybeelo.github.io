7de5c0a751593dd67ae2136b4def69f23ce2041d
Dan <d.elia@outlook.com>
2025-10-08 16:43:54 +0100

    Fix OpenGraph image linting issues

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from "next/og";
 
 export const dynamic = "force-static";
 
-export async function GET(request: Request) {
+export async function GET() {
 	const [font, monoFont] = await Promise.all([
 		fetch(
 			new URL(
@@ -16,22 +16,12 @@ export async function GET(request: Request) {
 		).then((res) => res.arrayBuffer()),
 	]);
 
-	const url = new URL(request.url);
-	const title = url.searchParams.get("title") || "Daniel Elia's Portfolio";
-	const image = await fetch(new URL("og_bg.jpg", url.origin)).then((res) =>
-		res.arrayBuffer()
-	);
+	const title = "Daniel Elia's Portfolio";
 
 	return new ImageResponse(
 		(
-			<img
-				src={image as any}
-				style={{
-					fontFamily: '"DM Sans"',
-				}}
-				tw="flex w-full h-full items-center justify-end blur-sm"
-			>
-				<div tw="flex md:flex-col py-12 px-4 md:items-left justify-between p-8 bg-[rgba(220,200,192,0.9)] rounded-lg m-10">
+			<div tw="flex w-full h-full items-center justify-center bg-[rgba(220,200,192,0.9)]">
+				<div tw="flex md:flex-col py-12 px-4 md:items-start justify-between p-8 bg-white rounded-lg m-10">
 					<h1 tw="flex flex-col text-5xl font-bold tracking-tight text-left">
 						{title}
 					</h1>
@@ -42,7 +32,7 @@ export async function GET(request: Request) {
 						donnybeelo.github.io
 					</p>
 				</div>
-			</img>
+			</div>
 		),
 		{
 			width: 1200,
