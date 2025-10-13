db937196e0630c0a82ccd691635d304356a52837
Dan <d.elia@outlook.com>
2025-10-13 12:29:05 +0100

    Fix styling for light mode

--- app/components/navButton.tsx
+++ app/components/navButton.tsx
@@ -28,7 +28,7 @@ export const NavButton = ({ path, name }: { path: string; name: string }) => {
 			className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1 navButton"
 			style={{
 				backgroundColor:
-					path === "/" + pathname.split("/")[1] ? "var(--button-outline)" : "",
+					path === "/" + pathname.split("/")[1] ? "var(--button-active)" : "",
 			}}
 		>
 			{name}
