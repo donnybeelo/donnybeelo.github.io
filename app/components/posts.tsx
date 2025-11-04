da459558a917543a15cdddb984d8ec5331e1a074
Dan <d.elia@outlook.com>
2025-11-04 02:00:26 +0000

    Fix styling of posts (mostly for mobile)

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -38,14 +38,15 @@ function Posts({
 				.map((post) => (
 					<ShinyButton
 						key={post.slug}
-						className="flex flex-col space-y-1 mb-4 shinyButton"
+						className="flex flex-col space-y-1 mb-4 shinyButton w-full"
 						path={`/${url}/${post.slug}`}
 					>
-						<div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+						<div className="w-full flex flex-row space-x-0 md:space-x-2">
 							<p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
 								{formatDate(post.metadata.publishedAt, false)}
 							</p>
-							<div className="flex-1">
+							<div className="inline-block w-[2px] !mx-2 bg-neutral-300 dark:bg-neutral-700 flex-shrink-0" />
+							<div className="flex flex-col">
 								<p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
 									{post.metadata.title}
 								</p>
