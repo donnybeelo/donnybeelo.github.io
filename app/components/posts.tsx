637d9046238040067b21899166cabaff8fa72ec4
Dan <d.elia@outlook.com>
2025-11-04 02:02:24 +0000

    Fix styling for desktop posts to fit to text once again

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -38,7 +38,7 @@ function Posts({
 				.map((post) => (
 					<ShinyButton
 						key={post.slug}
-						className="flex flex-col space-y-1 mb-4 shinyButton w-full"
+						className="flex flex-col space-y-1 mb-4 shinyButton md:w-fit w-full"
 						path={`/${url}/${post.slug}`}
 					>
 						<div className="w-full flex flex-row space-x-0 md:space-x-2">
