731bc6bc0c3b9c8e7cd67f0337225914a6325033
Dan <d.elia@outlook.com>
2026-01-06 01:01:56 +0000

    Add floating head of terror and doom (queue seance)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Typer from "./components/typer";
+import bauhaus from "@/public/bauhaus.svg";
 
 export default function Page() {
 	return (
-		<div className="h-[calc(100vh-22rem)] md:h-[calc(100vh-17rem)] flex flex-col justify-center items-start">
+		<div className="h-[calc(100vh-22rem)] md:h-[calc(100vh-17rem)] flex flex-col justify-center items-start relative">
 			<p className="mb-2 text-5xl font-semibold tracking-tighter">
 				Daniel Elia
 			</p>
@@ -10,6 +11,36 @@ export default function Page() {
 				I'm a developer based in Birmingham with a passion for making simple,
 				effective and beautiful user experiences.
 			</Typer>
+			<img
+				src={bauhaus.src}
+				className="absolute right-0 -translate-y-5 h-[calc(100vh-22rem)] md:h-[calc(100vh-17rem)] grayscale dark:invert select-none pointer-events-none animate-[float_6s_ease-in-out_infinite,opacity-pulse_3.7s_ease-in-out_infinite]"
+			/>
+			<style>{`
+				@keyframes float {
+					0%, 100% {
+						transform: translateY(0);
+					}
+					50% {
+						transform: translateY(1rem);
+					}
+				}
+				@keyframes opacity-pulse {
+					0%, 100% {
+						opacity: 0.08;
+					}
+					50% {
+						opacity: 0.05;
+					}
+				}
+				@keyframes float {
+					0%, 100% {
+						transform: translateY(0);
+					}
+					50% {
+						transform: translateY(1rem);
+					}
+				}
+			`}</style>
 		</div>
 	);
 }
