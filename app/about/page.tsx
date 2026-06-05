eefa677d4d366e338f1606bf5e724b2aa39a3c96
Dan <d.elia@outlook.com>
2026-06-05 01:00:36 +0100

    Update About page content and links

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,12 +4,21 @@ import Typer from "../components/typer";
 
 const content = `
 <Image className="float-end w-70 mt-0! ml-5! p-2!" src="/me.webp" />
-I'm a Computer Science graduate from the University of York based in Birmingham,
-experienced in full-stack development, UI/UX design, and cloud computing,
-working with organisations such as Sanger Institute and Genes & Health.
+I graduated in Computer Science from the University of York in 2025,
+and I'm currently working as a Full-stack Engineer at Thier.
+I specialise in UI/UX design and cloud computing,
+and have worked with Sanger Institute and Genes & Health in the past,
+where I brought my unique perspective to how software should be built,
+focusing on visual details and interactions.
 
-I specialise in collaborating directly with researchers to build tools that advance the technologies used in life sciences.
-I am a firm believer that great software is built on user studies, clear communication and agile teamwork, which led to achieving the certification of Scrum Master and Product Owner within 6 months.
+I am a firm believer that great software is built on user studies, clear communication and agile teamwork,
+which led me to become a certified Scrum Master and Product Owner with Scrum.org within 6 months.
+I contribute to open-source projects, such as [Geary](https://gitlab.gnome.org/GNOME/geary) and [Howdy](https://github.com/boltgolt/howdy),
+as well as being active within the GNOME Extensions community,
+creating such extensions as [GNOME Hotspot Toggle](/projects/hotspot-toggle) and [GLocalSend](/projects/glocalsend),
+and contributing to other projects such as [GTK4 DING](https://gitlab.com/smedius/desktop-icons-ng).
+I also enjoy building various tools and utilities,
+which you can find in my [projects](/projects) page.
 `;
 
 export default function Page() {
